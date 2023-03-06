1f44f8ddd7c4807e2a58437f1aa7558c453de121
warre <warre@LAPTOP-JM6U49HP>
2023-03-06 15:36:43 +1300

    routers

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,30 @@ import UseEffectComponent from './reactHooks/UseEffectComponent';
 import UseContextComponent from './reactHooks/UseContextComponent';
 import UseRefComponent from './reactHooks/UseRefComponent';
 import Messenger from './reactHooks/useReducer01/Messenger';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import * as ReactRouters from './reactRouters/ReactRouters';
+
 
 function App() {
 
   return (
-    <div>
-      {/* <UseStateComponet />
-      <UseEffectComponent /> 
-      <UseContextComponent />
-      <UseRefComponent />*/}
-      <Messenger />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ReactRouters.Layout />}>
+          <Route index element={<ReactRouters.Home />} />
+          <Route path="blogs" element={<ReactRouters.Blogs />} />
+          <Route path="contact" element={<ReactRouters.Contact />} />
+          <Route path="*" element={<ReactRouters.NoPage />} />
+        </Route>
+      {/* <div>
+        <UseStateComponet />
+        <UseEffectComponent /> 
+        <UseContextComponent />
+        <UseRefComponent />
+        <Messenger />
+      </div> */}
+      </Routes>
+    </BrowserRouter>
 
   );
 }
---
+++ src/reactRouters/ReactRouters.ts
@@ -0,0 +1,7 @@
+import {Home} from './Home'
+import {Blogs} from './Blogs'
+import {Contact} from './Contact'
+import {Layout} from './Layout'
+import {NoPage} from './NoPage'
+
+export {Home, Blogs, Contact, Layout, NoPage}
